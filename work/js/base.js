8c3de18ca4f8eeab3f2c56e3b9ef3e474dff5e01
liukun <liu_kun@msn.com>
2014-08-11 10:01:14 +0800

    功能基本告一段落

--- work/js/base.js
+++ work/js/base.js
@@ -1,15 +1,29 @@
 var host;
 var substringMatcher;
+
 $(document).ready(function ($) {
+
+    var argHost = $.request.queryString["host"];
+    var hostName = $.request.queryString["hostName"];
+
+    if ((argHost == undefined || argHost == "undefined") && window.location.pathname.indexOf("index.html") == -1) {//排除一些外部链接
+        top.location = "./index.html";
+    }
+//    alert( );
     $("#top a").each(function () {
-        $(this).attr("href", $(this).attr("href") + "?host=" + $.request.queryString["host"] + "&hostName=" + $.request.queryString["hostName"] +"&");
+        if( $(this).attr("href").indexOf("http:") != 0 ){
+            $(this).attr("href", $(this).attr("href") + "?host=" + argHost + "&hostName=" + hostName + "&");
+        }
     });
 
-    host = "http://" + $.request.queryString["host"] + "/";
+    host = "http://" + argHost + "/";
 
-    substringMatcher = function (strs) {
+    substringMatcher = function (strs, count) {
         return function findMatches(q, cb) {
 
+            if (count == undefined) {
+                count = 100000;
+            }
             var matches, substrRegex;
 
             // an array that will be populated with substring matches
@@ -24,12 +38,46 @@ $(document).ready(function ($) {
                 if (substrRegex.test(str)) {
                     // the typeahead jQuery plugin expects suggestions to a
                     // JavaScript object, refer to typeahead docs for more info
+
+
                     matches.push({ value: str });
+                    if (matches.length >= count) {
+                        //break;
+                        return false;
+                    }
                 }
             });
 
             cb(matches);
         };
     };
+
+    $("#hostName").text(hostName);
 //    alert( host );
+    var doc = document, inputs = doc.getElementsByTagName('input'), supportPlaceholder = 'placeholder'in doc.createElement('input'), placeholder = function (input) {
+        var text = input.getAttribute('placeholder'), defaultValue = input.defaultValue;
+        if (defaultValue == '') {
+            input.value = text
+        }
+        input.onfocus = function () {
+            if (input.value === text) {
+                this.value = ''
+            }
+        };
+        input.onblur = function () {
+            if (input.value === '') {
+                this.value = text
+            }
+        }
+    };
+    if (!supportPlaceholder) {
+        for (var i = 0, len = inputs.length; i < len; i++) {
+            var input = inputs[i], text = input.getAttribute('placeholder');
+            if (input.type === 'text' && text) {
+                placeholder(input)
+            }
+        }
+    }
+
+    jQuery.support.cors = true;
 });
