02039e4177a697ec29b508623c4c5403080854f0
liukun <liu_kun@msn.com>
2014-08-18 17:07:44 +0800

    功能基本告一段落

--- work/js/base.js
+++ work/js/base.js
@@ -1,6 +1,17 @@
 var host;
 var substringMatcher;
 
+function transfomDataToDHS(second) {
+    var day = Math.floor(second / (3600 * 24));
+    var hours = Math.floor((second - day * 3600 * 24) / 3600);
+    var scoends = Math.floor((second - day * 3600 * 24 - hours * 3600) / 60);
+    var str = "";
+    if (day > 0) str += day + "天";
+    if (hours > 0) str += hours + "小时";
+    str += scoends + "分";
+    return str;
+}
+
 $(document).ready(function ($) {
 
     var argHost = $.request.queryString["host"];
@@ -9,9 +20,11 @@ $(document).ready(function ($) {
     if ((argHost == undefined || argHost == "undefined") && window.location.pathname.indexOf("index.html") == -1) {//排除一些外部链接
         top.location = "./index.html";
     }
+
 //    alert( );
     $("#top a").each(function () {
-        if( $(this).attr("href").indexOf("http:") != 0 ){
+        var href = $(this).attr("href");
+        if (href != undefined && href.indexOf("http:") != 0 && href.indexOf("data") != 0) {
             $(this).attr("href", $(this).attr("href") + "?host=" + argHost + "&hostName=" + hostName + "&");
         }
     });
@@ -80,4 +93,12 @@ $(document).ready(function ($) {
     }
 
     jQuery.support.cors = true;
+
+    /**
+     *把秒转换为 多少天 多少小时 多少分
+     * @return
+     *
+     */
+
+
 });
